44336281172266fa0e9bb9afc34c52a425a88f99
Scott Kao <scottxxx666@gmail.com>
2019-07-27 03:45:45 +0800

    v2: map

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -4,12 +4,13 @@
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
+  let map = {};
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if ((nums[i] + nums[j]) === target) {
-        return [i, j];
-      }
+    const diff = target - nums[i];
+    if (diff in map) {
+      return [map[diff], i]
     }
+    map[nums[i]] = i
   }
 };
 
