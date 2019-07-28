df2dfdf29bf5ded8650b4c30638330c15db41136
Scott Kao <scottxxx666@gmail.com>
2019-07-28 16:49:50 +0800

    Fix assert

--- reverseInteger/reverseInteger.test.js
+++ reverseInteger/reverseInteger.test.js
@@ -1,29 +1,29 @@
 const reverseInteger = require('./reverseInteger');
 
 test('1 digit return same number', () => {
-  expect(reverseInteger(1)).toStrictEqual(1);
+  expect(reverseInteger(1)).toEqual(1);
 });
 
 test('12 return 21', () => {
-  expect(reverseInteger(12)).toStrictEqual(21);
+  expect(reverseInteger(12)).toEqual(21);
 });
 
 test('123 return 321', () => {
-  expect(reverseInteger(123)).toStrictEqual(321);
+  expect(reverseInteger(123)).toEqual(321);
 });
 
 test('-12 return same -21', () => {
-  expect(reverseInteger(-12)).toStrictEqual(-21);
+  expect(reverseInteger(-12)).toEqual(-21);
 })
 
 test('10 return 1', () => {
-  expect(reverseInteger(10)).toStrictEqual(1);
+  expect(reverseInteger(10)).toEqual(1);
 })
 
 test('overflow 2^31 should return 0', () => {
-  expect(reverseInteger(1534236469)).toStrictEqual(0);
+  expect(reverseInteger(1534236469)).toEqual(0);
 })
 
 test('overflow -2^31 should return 0', () => {
-  expect(reverseInteger(-1534236469)).toStrictEqual(0);
+  expect(reverseInteger(-1534236469)).toEqual(0);
 })
