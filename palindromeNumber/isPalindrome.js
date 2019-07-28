d405e66295ecddd042f9fa2af1ffc370b0a4c454
Scott Kao <scottxxx666@gmail.com>
2019-07-28 17:04:16 +0800

    ToString version

--- palindromeNumber/isPalindrome.js
+++ palindromeNumber/isPalindrome.js
@@ -2,17 +2,9 @@ module.exports = x => {
   if (x < 0) {
     return false;
   }
-  let digits = 0;
-  let y = x;
-  while (x != 0) {
-    digits += 1;
-    x = parseInt(x / 10);
-  }
-  for (let i = 0; i < (digits / 2); i = i + 1) {
-    const a = y % 10;
-    const b = parseInt(y / Math.pow(10, digits - 2 * i - 1)) % 10;
-    y = parseInt(y / 10)
-    if (a !== b) {
+  let input = x.toString();
+  for (let i = 0; i < input.length / 2; i++) {
+    if (input[i] !== input[input.length - i - 1]) {
       return false;
     }
   }
