425780a60331c3767262219283588324f56454cf
Scott Kao <scottxxx666@gmail.com>
2019-07-28 17:30:17 +0800

    Compare reverted number

--- palindromeNumber/isPalindrome.js
+++ palindromeNumber/isPalindrome.js
@@ -2,11 +2,14 @@ module.exports = x => {
   if (x < 0) {
     return false;
   }
-  let input = x.toString();
-  for (let i = 0; i < input.length / 2; i++) {
-    if (input[i] !== input[input.length - i - 1]) {
-      return false;
+  const origin = x;
+  let reverted = 0;
+  while (x !== 0) {
+    reverted += x % 10;
+    x = parseInt(x / 10);
+    if (x !== 0) {
+      reverted = reverted * 10;
     }
   }
-  return true
+  return origin === reverted
 }
