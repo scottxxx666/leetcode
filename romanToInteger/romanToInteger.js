8f81462748fb321c36e227f3cf73f2b9492c0c5e
Scott Kao <scottxxx666@gmail.com>
2019-07-29 16:52:05 +0800

    Replace substraction

--- romanToInteger/romanToInteger.js
+++ romanToInteger/romanToInteger.js
@@ -2,12 +2,8 @@ module.exports = s => {
   const intMap = {
     'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
   };
-  let result = 0;
-  for (let i = 0; i < s.length; i++) {
-    result += intMap[s[i]];
-    if (i !== 0 && intMap[s[i]] > intMap[s[i - 1]]) {
-      result -= intMap[s[i - 1]] * 2;
-    }
-  }
-  return result;
+  s = s.replace('IV', 'IIII').replace('IX', 'IIIIV');
+  s = s.replace('XL', 'XXXX').replace('XC', 'XXXXL');
+  s = s.replace('CD', 'CCCC').replace('CM', 'CCCCD');
+  return s.split('').reduce((a, b) => a + intMap[b], 0);
 };
