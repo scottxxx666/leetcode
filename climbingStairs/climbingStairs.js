9f5349b4830dd6dcd26519bea784e01e08e9edd1
Scott Kao <scottxxx666@gmail.com>
2019-08-07 03:23:55 +0800

    Normal solution

--- climbingStairs/climbingStairs.js
+++ climbingStairs/climbingStairs.js
@@ -1,10 +1,16 @@
 function calculateN(n) {
-  const square5 = Math.sqrt(5);
-  return square5 / 5 * (Math.pow((1 + square5) / 2, n) - Math.pow((1 - square5) / 2, n));
+  let last = 0;
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    const temp = result;
+    result += last;
+    last = temp;
+  }
+  return result;
 }
 
 const climbStairs = function (n) {
-  return Math.round(calculateN(n) + calculateN(n - 1));
+  return calculateN(n);
 };
 
 module.exports = climbStairs;
