903c0ba73e17ed84c68bc08baa59a536f91a7e89
Scott Kao <scottxxx666@gmail.com>
2019-08-07 03:30:19 +0800

    Remove temp variable and inline function

--- climbingStairs/climbingStairs.js
+++ climbingStairs/climbingStairs.js
@@ -1,16 +1,11 @@
-function calculateN(n) {
+const climbStairs = function (n) {
   let last = 0;
   let result = 1;
   for (let i = 1; i <= n; i++) {
-    const temp = result;
     result += last;
-    last = temp;
+    last = result - last;
   }
   return result;
-}
-
-const climbStairs = function (n) {
-  return calculateN(n);
 };
 
 module.exports = climbStairs;
