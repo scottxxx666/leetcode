5e55175853cc7f1109d989e8daf7b0b34b6c04ac
Scott Kao <scottxxx666@gmail.com>
2019-08-07 02:56:11 +0800

    Formula solution

--- climbingStairs/climbingStairs.js
+++ climbingStairs/climbingStairs.js
@@ -1,9 +1,10 @@
-const climbStairs = function (n) {
+function calculateN(n) {
+  const square5 = Math.sqrt(5);
+  return square5 / 5 * (Math.pow((1 + square5) / 2, n) - Math.pow((1 - square5) / 2, n));
+}
 
-  if (n <= 2) {
-    return n;
-  }
-  return climbStairs(n - 1) + climbStairs(n - 2);
+const climbStairs = function (n) {
+  return Math.round(calculateN(n) + calculateN(n - 1));
 };
 
 module.exports = climbStairs;
