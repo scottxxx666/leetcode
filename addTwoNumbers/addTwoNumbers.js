31fdb1879cdf9322d41e332c8e27443d6186ec5c
Scott Kao <scottxxx666@gmail.com>
2019-08-09 14:57:58 +0800

    Refactor

--- addTwoNumbers/addTwoNumbers.js
+++ addTwoNumbers/addTwoNumbers.js
@@ -3,7 +3,7 @@ const addTwoNumbers = function (l1, l2) {
   let result = new ListNode();
   let pointer = result;
   let carry = 0;
-  while (l1 !== null || l2 !== null) {
+  while (l1 !== null || l2 !== null || carry !== 0) {
     let val1 = l1 === null ? 0 : l1.val;
     const val2 = l2 === null ? 0 : l2.val;
     val1 += val2 + carry;
@@ -22,9 +22,6 @@ const addTwoNumbers = function (l1, l2) {
     if (l2 !== null) {
       l2 = l2.next;
     }
-  }
-  if (carry !== 0) {
-    pointer.next = new ListNode(carry);
   }
   return result.next;
 };
