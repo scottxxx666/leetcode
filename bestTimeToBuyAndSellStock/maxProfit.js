d895f0114cdc22d8ac55724a2730c399d6f1f3c9
Scott Kao <scottxxx666@gmail.com>
2019-08-10 14:39:50 +0800

    O(n) solution

--- bestTimeToBuyAndSellStock/maxProfit.js
+++ bestTimeToBuyAndSellStock/maxProfit.js
@@ -1,11 +1,13 @@
 module.exports = function (prices) {
   let minBefore = Number.MAX_SAFE_INTEGER;
+  let max = 0;
   for (let i = 0; i < prices.length; i++) {
-    const price = prices[i];
-    prices[i] = price - minBefore;
-    if (price < minBefore) {
-      minBefore = price;
+    if (prices[i] < minBefore) {
+      minBefore = prices[i];
+    }
+    if (prices[i] - minBefore > max) {
+      max = prices[i] - minBefore;
     }
   }
-  return Math.max(0, ...prices);
+  return max;
 };
