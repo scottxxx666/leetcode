19703d9d20daefef401774cf13a5671e121d4cbb
Scott Kao <scottxxx666@gmail.com>
2019-08-12 17:11:23 +0800

    Refactor for less if

--- longestSubstringWithoutRepeatingChar/lengthOfLongestSubstring.js
+++ longestSubstringWithoutRepeatingChar/lengthOfLongestSubstring.js
@@ -4,11 +4,10 @@ module.exports = function (s) {
   let map = new Map();
   for (let i = 0; i < s.length; i++) {
     const each = s[i];
-    if (!map.has(each) || map.get(each) < start) {
-      result = Math.max(result, i - start + 1);
-    } else {
-      start = map.get(each) + 1;
+    if (map.has(each)) {
+      start = Math.max(map.get(each) + 1, start);
     }
+    result = Math.max(result, i - start + 1);
     map.set(each, i);
   }
   return result;
