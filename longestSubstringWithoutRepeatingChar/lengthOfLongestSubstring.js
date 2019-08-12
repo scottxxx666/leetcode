ea26241f24d30a82dcb89357ff68c17975a9abcb
Scott Kao <scottxxx666@gmail.com>
2019-08-12 16:55:10 +0800

    O(n) solution

--- longestSubstringWithoutRepeatingChar/lengthOfLongestSubstring.js
+++ longestSubstringWithoutRepeatingChar/lengthOfLongestSubstring.js
@@ -1,18 +1,15 @@
 module.exports = function (s) {
   let result = 0;
+  let start = 0;
+  let map = new Map();
   for (let i = 0; i < s.length; i++) {
-    let existBefore = new Set();
-    for (let j = i; j < s.length; j++) {
-      const each = s[j];
-      if (!existBefore.has(each)) {
-        existBefore.add(each);
-      } else {
-        existBefore = new Set([each]);
-      }
-      if (existBefore.size > result) {
-        result = existBefore.size;
-      }
+    const each = s[i];
+    if (!map.has(each) || map.get(each) < start) {
+      result = Math.max(result, i - start + 1);
+    } else {
+      start = map.get(each) + 1;
     }
+    map.set(each, i);
   }
   return result;
 };
