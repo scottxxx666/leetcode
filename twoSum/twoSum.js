5d342cb5ab2d5c7c8dfadab64b740dc49ad5213a
Scott Kao <scottxxx666@gmail.com>
2019-08-14 23:55:13 +0800

    Refactor: Map instead object

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  let map = {};
+  let map = new Map();
   for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
-    if (diff in map) {
-      return [map[diff], i]
+    if (map.has(diff)) {
+      return [map.get(diff), i]
     }
-    map[nums[i]] = i
+    map.set(nums[i], i);
   }
 };
 
