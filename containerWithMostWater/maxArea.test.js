2827b0619a70fbd7ee8828fbd8c5ca07a8ae6421
Scott Kao <scottxxx666@gmail.com>
2019-08-16 00:00:28 +0800

    Fix test

--- containerWithMostWater/maxArea.test.js
+++ containerWithMostWater/maxArea.test.js
@@ -1,29 +1,29 @@
 const maxArea = require('./maxArea');
 
-test('Only 1 duplicate number in array', () => {
+test('Only 2 integer', () => {
   expect(maxArea([1, 1])).toEqual(1);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('Only 2 integer with height 2', () => {
   expect(maxArea([2, 2])).toEqual(2);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('Only 2 integer with diff height', () => {
   expect(maxArea([1, 2])).toEqual(1);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('3 same integer', () => {
   expect(maxArea([1, 1, 1])).toEqual(2);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('Head and tail are highest', () => {
   expect(maxArea([2, 1, 2])).toEqual(4);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('First and second are highest', () => {
   expect(maxArea([3, 3, 1])).toEqual(3);
 });
 
-test('Only 1 duplicate number in array', () => {
+test('2 highest in middle', () => {
   expect(maxArea([1, 4, 4, 1])).toEqual(4);
 });
