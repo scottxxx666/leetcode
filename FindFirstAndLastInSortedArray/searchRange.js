eaa0a7a1fa55b374ab4517cc1db4f1e8063fb882
Scott Kao <scottxxx666@gmail.com>
2019-08-22 00:12:24 +0800

    Merge search

--- FindFirstAndLastInSortedArray/searchRange.js
+++ FindFirstAndLastInSortedArray/searchRange.js
@@ -1,40 +1,34 @@
-function searchLeft(nums, target) {
+function search(nums, target, direction) {
   let start = 0;
   let end = nums.length - 1;
   while (start <= end) {
     const pointer = start + Math.floor((end - start) / 2);
 
-    if (nums[pointer] === target && (pointer === start || nums[pointer - 1] < target)) {
-      return pointer;
-    } else if (nums[pointer] < target) {
-      start = pointer + 1;
+    if (direction === 'left') {
+      if (nums[pointer] === target && (pointer === start || nums[pointer - 1] !== target)) {
+        return pointer;
+      }
+      if (nums[pointer] < target) {
+        start = pointer + 1;
+      } else {
+        end = pointer - 1;
+      }
     } else {
-      end = pointer - 1;
-    }
-
-  }
-  return -1;
-}
-
-function searchRight(nums, target) {
-  let start = 0;
-  let end = nums.length - 1;
-  while (start <= end) {
-    const pointer = start + Math.floor((end - start) / 2);
-    if (nums[pointer] === target && (pointer === end || nums[pointer + 1] > target)) {
-      return pointer;
-    }
-    if (nums[pointer] > target) {
-      end = pointer - 1;
-    } else {
-      start = pointer + 1;
+      if (nums[pointer] === target && (pointer === end || nums[pointer + 1] !== target)) {
+        return pointer;
+      }
+      if (nums[pointer] > target) {
+        end = pointer - 1;
+      } else {
+        start = pointer + 1;
+      }
     }
   }
   return -1;
 }
 
 const searchRange = function (nums, target) {
-  return [searchLeft(nums, target), searchRight(nums, target)];
+  return [search(nums, target, 'left'), search(nums, target, 'right')];
 };
 
 module.exports = searchRange;
