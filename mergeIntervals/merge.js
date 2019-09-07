3d40d899c6cb10cb2d25452c03093bb8851bacfd
Scott Kao <scottxxx666@gmail.com>
2019-09-08 03:16:56 +0800

    nlogn solution

--- mergeIntervals/merge.js
+++ mergeIntervals/merge.js
@@ -2,21 +2,19 @@ function merge(intervals) {
   if (intervals.length === 0) {
     return intervals;
   }
-  let result = [intervals[0]];
-  for (let i = 1; i < intervals.length; i++) {
-    let begin = intervals[i][0];
-    let end = intervals[i][1];
-    const newResult = [];
-    for (let j = 0; j < result.length; j++) {
-      if ((begin < result[j][0] && end < result[j][0]) || (begin > result[j][1] && end > result[j][1])) {
-        newResult.push([result[j][0], result[j][1]]);
-      } else {
-        begin = Math.min(begin, result[j][0]);
-        end = Math.max(end, result[j][1]);
-      }
+  let sorted = intervals.sort((a, b) => a[0] - b[0]);
+  let result = [sorted[0]];
+  for (let i = 1; i < sorted.length; i++) {
+    let begin = sorted[i][0];
+    let end = sorted[i][1];
+    const last = result[(result.length - 1)];
+    if ((begin < last[0] && end < last[0]) || (begin > last[1] && end > last[1])) {
+      result.push([begin, end]);
+    } else {
+      begin = Math.min(begin, last[0]);
+      end = Math.max(end, last[1]);
+      result[result.length - 1] = [begin, end];
     }
-    newResult.push([begin, end]);
-    result = newResult;
   }
   return result;
 }
