f24d62e4ccf7d2ac6ca1b7c8c48efc23e234c8c2
Scott Kao <scottxxx666@gmail.com>
2019-09-09 17:47:23 +0800

    DP solution

--- minimumPathSum/minPathSum.js
+++ minimumPathSum/minPathSum.js
@@ -1,16 +1,15 @@
-function minPathSum(grid, m = 0, n = 0) {
-  if (m === grid.length - 1 && n === grid[0].length - 1) {
-    return grid[m][n];
+function minPathSum(grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i === 0 && j === 0) {
+        continue;
+      }
+      let top = i === 0 ? Number.POSITIVE_INFINITY : grid[i - 1][j];
+      let left = j === 0 ? Number.POSITIVE_INFINITY : grid[i][j - 1];
+      grid[i][j] += Math.min(top, left);
+    }
   }
-  let right = Number.POSITIVE_INFINITY;
-  let down = Number.POSITIVE_INFINITY;
-  if (m < grid.length - 1) {
-    right = minPathSum(grid, m + 1, n);
-  }
-  if (n < grid[0].length - 1) {
-    down = minPathSum(grid, m, n + 1);
-  }
-  return grid[m][n] + Math.min(right, down);
+  return grid[grid.length - 1][grid[0].length - 1];
 }
 
 module.exports = minPathSum;
