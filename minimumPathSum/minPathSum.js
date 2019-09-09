60136c86815c4321cadb87971d6ea64516160ad3
Scott Kao <scottxxx666@gmail.com>
2019-09-10 00:50:01 +0800

    Refactor: replace init for loop

--- minimumPathSum/minPathSum.js
+++ minimumPathSum/minPathSum.js
@@ -1,9 +1,7 @@
 function minPathSum(grid) {
-  let steps = [...grid[0]];
-  for (let i = 1; i < steps.length; i++) {
-    steps[i] += steps[i - 1];
-  }
-  for (let i = 1; i < grid.length; i++) {
+  let steps = new Array(grid[0].length).fill(Number.POSITIVE_INFINITY);
+  steps[0] = 0;
+  for (let i = 0; i < grid.length; i++) {
     steps[0] += grid[i][0];
     for (let j = 1; j < steps.length; j++) {
       steps[j] = grid[i][j] + Math.min(steps[j - 1], steps[j]);
