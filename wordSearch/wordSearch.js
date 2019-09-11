9d588a4d75a9038bfdf0e6d29abbab0e626bd262
Scott Kao <scottxxx666@gmail.com>
2019-09-12 03:19:06 +0800

    Refactor: remove result object

--- wordSearch/wordSearch.js
+++ wordSearch/wordSearch.js
@@ -1,34 +1,31 @@
-function search(result, board, word, i, j, wordIndex) {
-  if (result.find === true) {
-    return;
-  }
+function search(board, word, i, j, wordIndex) {
   if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
-    return;
+    return false;
   }
   if (board[i][j] !== word[wordIndex]) {
-    return;
+    return false;
   }
   if (wordIndex === word.length - 1) {
-    result.find = true;
-    return;
+    return true;
   }
   const temp = board[i][j];
   board[i][j] = '';
-  search(result, board, word, i - 1, j, wordIndex + 1);
-  search(result, board, word, i + 1, j, wordIndex + 1);
-  search(result, board, word, i, j - 1, wordIndex + 1);
-  search(result, board, word, i, j + 1, wordIndex + 1);
+  const exist = search(board, word, i - 1, j, wordIndex + 1) ||
+    search(board, word, i + 1, j, wordIndex + 1) ||
+    search(board, word, i, j - 1, wordIndex + 1) ||
+    search(board, word, i, j + 1, wordIndex + 1);
   board[i][j] = temp;
+  return exist;
 }
 
 function wordSearch(board, word) {
-  let result = {'find': false};
+  let result = false;
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      search(result, board, word, i, j, 0);
+      result = result || search(board, word, i, j, 0);
     }
   }
-  return result.find;
+  return result;
 }
 
 module.exports = wordSearch;
