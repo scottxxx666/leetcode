445e9fa7c17d42bbbde9eae6af83b8fa3473206a
Scott Kao <scottxxx666@gmail.com>
2019-09-11 17:20:09 +0800

    Refactor: readable

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -1,14 +1,14 @@
 function nextSubsets(nums, result, subset, start) {
+  result.push([...subset]);
   for (let i = start; i < nums.length; i++) {
     subset.push(nums[i]);
-    result.push([...subset]);
     nextSubsets(nums, result, subset, i + 1)
     subset.pop();
   }
 }
 
 function subsets(nums) {
-  const result = [[]];
+  const result = [];
   const subset = [];
   nextSubsets(nums, result, subset, 0);
   return result;
