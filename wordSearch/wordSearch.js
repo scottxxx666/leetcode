fb370b63535132cc97de0e1411defb7097e8764d
Scott Kao <scottxxx666@gmail.com>
2019-09-12 03:10:01 +0800

    Refactor: remove set

--- wordSearch/wordSearch.js
+++ wordSearch/wordSearch.js
@@ -1,34 +1,31 @@
-function search(result, board, word, set, i, j, wordIndex) {
+function search(result, board, word, i, j, wordIndex) {
   if (result.find === true) {
     return;
   }
   if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
     return;
   }
-  const key = [i, j].join(',');
-  if (set.has(key)) {
-    return;
-  }
   if (board[i][j] !== word[wordIndex]) {
     return;
   }
-  if (set.size === word.length - 1) {
+  if (wordIndex === word.length - 1) {
     result.find = true;
     return;
   }
-  set.add(key);
-  search(result, board, word, set, i - 1, j, wordIndex + 1);
-  search(result, board, word, set, i + 1, j, wordIndex + 1);
-  search(result, board, word, set, i, j - 1, wordIndex + 1);
-  search(result, board, word, set, i, j + 1, wordIndex + 1);
-  set.delete(key);
+  const temp = board[i][j];
+  board[i][j] = '';
+  search(result, board, word, i - 1, j, wordIndex + 1);
+  search(result, board, word, i + 1, j, wordIndex + 1);
+  search(result, board, word, i, j - 1, wordIndex + 1);
+  search(result, board, word, i, j + 1, wordIndex + 1);
+  board[i][j] = temp;
 }
 
 function wordSearch(board, word) {
   let result = {'find': false};
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      search(result, board, word, new Set(), i, j, 0);
+      search(result, board, word, i, j, 0);
     }
   }
   return result.find;
