656b6843da6363150b468116642184af7fa4f734
Scott Kao <scottxxx666@gmail.com>
2019-09-13 03:21:06 +0800

    DP solution

--- wordBreak/wordBreak.js
+++ wordBreak/wordBreak.js
@@ -2,17 +2,17 @@ function match(s, word, sIndex) {
   return s.substring(sIndex, sIndex + word.length).includes(word);
 }
 
-function wordBreak(s, wordDict, sIndex = 0) {
-  let result = false;
-  if (sIndex === s.length) {
-    return true;
-  }
-  for (let word of wordDict) {
-    if (match(s, word, sIndex)) {
-      result = result || wordBreak(s, wordDict, sIndex + word.length);
+function wordBreak(s, wordDict) {
+  const dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 0; i < s.length; i++) {
+    for (let word of wordDict) {
+      if (match(s, word, i) && dp[i]) {
+        dp[i + word.length] = true;
+      }
     }
   }
-  return result;
+  return dp[dp.length - 1];
 }
 
 
