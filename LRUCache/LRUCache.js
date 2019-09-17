bc66b2d5360015182b06791ec6baf0a0d00d5ea1
Scott Kao <scottxxx666@gmail.com>
2019-09-17 17:17:22 +0800

    Brilliant javascript map iterator solution

--- LRUCache/LRUCache.js
+++ LRUCache/LRUCache.js
@@ -1,69 +1,30 @@
 function LRUCache(capacity) {
   this.capacity = capacity;
-  this.linkList = new LinkList();
   this.map = new Map();
+  this.iter = this.map[Symbol.iterator]();
 }
-
-const Node = function (key = null, val = null) {
-  this.key = key;
-  this.val = val;
-  this.next = null;
-  this.prev = null;
-};
-
-const LinkList = function () {
-  this.head = new Node();
-  this.tail = new Node();
-  this.head.next = this.tail;
-  this.tail.prev = this.head;
-};
-
-LinkList.prototype.add = function (node) {
-  node.prev = this.tail.prev;
-  node.next = this.tail;
-  this.tail.prev.next = node;
-  this.tail.prev = node;
-};
-
-LinkList.prototype.remove = function () {
-  const target = this.head.next;
-  this.head.next = target.next;
-  target.next.prev = this.head;
-  return target;
-};
-
-LinkList.prototype.moveToEnd = function (node) {
-  node.prev.next = node.next;
-  node.next.prev = node.prev;
-  this.add(node);
-};
 
 LRUCache.prototype.get = function (key) {
   if (!this.map.has(key)) {
     return -1;
   }
-  const node = this.map.get(key);
-  this.linkList.moveToEnd(node);
-
-  return node.val;
+  const value = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, value);
+  return value;
 };
 
 LRUCache.prototype.put = function (key, value) {
   if (this.map.has(key)) {
-    const node = this.map.get(key);
-    node.val = value;
-    this.linkList.moveToEnd(node);
+    this.map.delete(key);
+    this.map.set(key, value);
     return;
   }
   if (this.map.size === this.capacity) {
-    const removed = this.linkList.remove();
-    if (removed) {
-      this.map.delete(removed.key);
-    }
+    const next = this.iter.next();
+    this.map.delete(next.value[0]);
   }
-  const node = new Node(key, value);
-  this.map.set(key, node);
-  this.linkList.add(node);
+  this.map.set(key, value);
 };
 
 module.exports = LRUCache;
