3a04d1a64996bb5205db7ddc5a3e1723ee50c3b0
Scott Kao <scottxxx666@gmail.com>
2019-09-18 23:10:21 +0800

    Refactor: constant space

--- maximunProductSubarray/maxProduct.js
+++ maximunProductSubarray/maxProduct.js
@@ -1,13 +1,12 @@
 maxProduct = function (nums) {
-  let products = new Array(nums.length).fill({});
-  products[0] = {max: nums[0], min: nums[0]};
   let max = nums[0];
+  let localMax = nums[0];
+  let localMin = nums[0];
   for (let i = 1; i < nums.length; i++) {
-    products[i] = {
-      max: Math.max(products[i - 1].max * nums[i], products[i - 1].min * nums[i], nums[i]),
-      min: Math.min(products[i - 1].min * nums[i], products[i - 1].max * nums[i], nums[i])
-    };
-    max = Math.max(max, products[i].max);
+    const temp = Math.max(localMax * nums[i], localMin * nums[i], nums[i]);
+    localMin = Math.min(localMax * nums[i], localMin * nums[i], nums[i]);
+    localMax = temp;
+    max = Math.max(max, localMax, localMin);
   }
   return max;
 };
