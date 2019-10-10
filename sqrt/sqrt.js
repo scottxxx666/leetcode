20f75dd3c8382df14b7ce93e10437f4d5cae5d09
Scott Kao <scottxxx666@gmail.com>
2019-10-11 02:41:14 +0800

    Binary search

--- sqrt/sqrt.js
+++ sqrt/sqrt.js
@@ -1,8 +1,17 @@
 module.exports = function (x) {
-  let result = 0;
-  while (result ** 2 <= x) {
-    result++;
+  let min = 0;
+  let max = x;
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    if (mid ** 2 === x) {
+      return mid;
+    }
+    if (mid ** 2 > x) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
+    }
   }
-  return result - 1;
+  return max;
 };
 
