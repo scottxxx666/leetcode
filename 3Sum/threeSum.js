6682f00bd1e787b20198320fc2f42a823d5c05eb
Scott Kao <scottxxx666@gmail.com>
2019-10-14 18:16:28 +0800

    n**2 logn solution

--- 3Sum/threeSum.js
+++ 3Sum/threeSum.js
@@ -1,15 +1,28 @@
+function find(nums, number, start) {
+  let low = start;
+  let high = nums.length - 1;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (nums[mid] === number) {
+      return mid;
+    }
+    if (nums[mid] > number) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+
 module.exports = function (nums) {
   const input = nums.sort((a, b) => a - b);
   const map = new Map();
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; j < input.length; j++) {
-      for (let k = j + 1; k < input.length; k++) {
-        if (input[i] + input[j] + input[k] === 0) {
-          map.set(`${input[i]},${input[j]},${input[k]}`, 1);
-        }
-        if (input[i] + input[j] + input[k] >= 0) {
-          break;
-        }
+      const target = find(nums, -input[i] - input[j], j + 1);
+      if (target !== -1) {
+        map.set(`${input[i]},${input[j]},${input[target]}`, 1);
       }
     }
   }
