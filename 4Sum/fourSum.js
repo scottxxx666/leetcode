cc89716129fceac1b4902f96ff82c4777abc8e73
Scott Kao <scottxxx666@gmail.com>
2019-10-16 16:32:11 +0800

    Refactor: readable

--- 4Sum/fourSum.js
+++ 4Sum/fourSum.js
@@ -1,8 +1,14 @@
 module.exports = function (nums, target) {
   const input = nums.sort((a, b) => a - b);
   const result = [];
-  for (let i = 0; i < input.length;) {
-    for (let j = i + 1; j < input.length;) {
+  for (let i = 0; i < input.length - 2; i++) {
+    if (i > 0 && input[i] === input[i - 1]) {
+      continue;
+    }
+    for (let j = i + 1; j < input.length - 1; j++) {
+      if (j > i + 1 && input[j] === input[j - 1]) {
+        continue;
+      }
       let low = j + 1;
       let high = input.length - 1;
       while (low < high) {
@@ -23,15 +29,6 @@ module.exports = function (nums, target) {
           low++;
         }
       }
-
-      const last = input[j];
-      while (j < input.length && input[j] === last) {
-        j++;
-      }
-    }
-    const last = input[i];
-    while (i < input.length && input[i] === last) {
-      i++;
     }
   }
   return result;
