7f871b3897cc6c27441d67be6be95d5cd83cdd69
Scott Kao <scottxxx666@gmail.com>
2019-10-17 17:10:08 +0800

    Merge comparision

--- validSudoku/validSudoku.js
+++ validSudoku/validSudoku.js
@@ -1,48 +1,20 @@
 module.exports = function (board) {
-  const allNums = { '1': true, '2': true, '3': true, '4': true, '5': true, '6': true, '7': true, '8': true, '9': true };
+  const map = new Map();
   for (let i = 0; i < board.length; i++) {
-    const nums = { ...allNums };
     for (let j = 0; j < board[i].length; j++) {
       const target = board[i][j];
       if (target === '.') {
         continue;
       }
-      if (!nums.hasOwnProperty(target)) {
+      const rowKey = `${target}-row-${i}`;
+      const colKey = `${target}-col-${j}`;
+      const boxKey = `${target}-box-${Math.floor(i / 3)},${Math.floor(j / 3)}`;
+      if (map.has(rowKey) || map.has(colKey) || map.has(boxKey)) {
         return false;
       }
-      delete nums[target];
-    }
-  }
-
-  for (let i = 0; i < board[0].length; i++) {
-    const nums = { ...allNums };
-    for (let j = 0; j < board.length; j++) {
-      const target = board[j][i];
-      if (target === '.') {
-        continue;
-      }
-      if (!nums.hasOwnProperty(target)) {
-        return false;
-      }
-      delete nums[target];
-    }
-  }
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      const nums = { ...allNums };
-      for (let k = 0; k < 3; k++) {
-        for (let l = 0; l < 3; l++) {
-          const target = board[3 * i + k][3 * j + l];
-          if (target === '.') {
-            continue;
-          }
-          if (!nums.hasOwnProperty(target)) {
-            return false;
-          }
-          delete nums[target];
-        }
-      }
+      map.set(rowKey, true);
+      map.set(colKey, true);
+      map.set(boxKey, true);
     }
   }
   return true;
