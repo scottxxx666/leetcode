1ee709a7ab438da4fa681d322583ae29400993bf
Scott Kao <scottxxx666@gmail.com>
2019-10-20 17:07:16 +0800

    Declare result as zero filled

--- multiplyStrings/multiplyStrings.js
+++ multiplyStrings/multiplyStrings.js
@@ -6,26 +6,19 @@ function toInt(num, i) {
 }
 
 module.exports = function (num1, num2) {
-  if (num1 === '0' || num2 === '0') {
-    return '0';
-  }
-  let sums = [];
+  let sums = new Array(num1.length + num2.length).fill(0);
   for (let i = num1.length - 1; i >= 0; i--) {
     for (let j = num2.length - 1; j >= 0; j--) {
-      let product = toInt(num1, i) * toInt(num2, j);
-      let key = num1.length - 1 - i + num2.length - 1 - j;
-      do {
-        if (key >= sums.length) {
-          sums.push(product);
-        } else {
-          sums[key] += product;
-        }
-        product = Math.floor(sums[key] / 10);
-        sums[key] = sums[key] % 10;
-        key++;
-      } while (product !== 0)
+      let key = i + j + 1;
+      let product = toInt(num1, i) * toInt(num2, j) + sums[key];
+      sums[key] = product % 10;
+      sums[key - 1] += Math.floor(product / 10);
     }
   }
-  return sums.reverse().join('');
+  let start = false;
+  return sums.reduce((prev, cur) => {
+    start = cur !== 0 ? true : start;
+    return start ? prev + cur : '';
+  }, '') || '0';
 };
 
