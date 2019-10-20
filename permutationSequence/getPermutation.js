baba0d51b536f7b17bd2d8a876e7363c88c6effc
Scott Kao <scottxxx666@gmail.com>
2019-10-21 03:37:08 +0800

    Loop solution and save factorial

--- permutationSequence/getPermutation.js
+++ permutationSequence/getPermutation.js
@@ -1,29 +1,22 @@
-function nextPermutation(n) {
-  let result = 1;
-  while (n > 1) {
-    result *= n;
-    n--;
-  }
-  return result;
-}
-
-function permutation(n, k, solution, set) {
-  if (n === 1) {
-    solution.push(...set);
-    return;
-  }
-  const next = nextPermutation(n - 1);
-  const key = k === 0 ? n : Math.ceil(k / next);
-  const digit = [...set][key - 1];
-  solution.push(digit);
-  set.delete(digit);
-  permutation(n - 1, k % next, solution, set);
-}
-
 module.exports = function (n, k) {
   const set = new Set(Array(n).fill(0).map((value, key) => key + 1));
+  const factorial = new Array(n);
+  let temp = 1;
+  factorial[0] = 1;
+  for (let i = 1; i < factorial.length; i++) {
+    temp *= i;
+    factorial[i] = temp;
+  }
+
+  let key = k;
   const solution = [];
-  permutation(n, k, solution, set);
+  for (let i = n; i > 0; i--) {
+    key = k === 0 ? i : Math.ceil(k / factorial[i - 1]);
+    const digit = [...set][key - 1];
+    solution.push(digit);
+    set.delete(digit);
+    k = k % factorial[i - 1];
+  }
   return solution.join('');
 };
 
