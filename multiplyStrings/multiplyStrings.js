799709363d420bb6a5881c6045daf2b0ecd845f1
Scott Kao <scottxxx666@gmail.com>
2019-10-20 16:19:57 +0800

    Direct count each product directly

--- multiplyStrings/multiplyStrings.js
+++ multiplyStrings/multiplyStrings.js
@@ -2,7 +2,7 @@ function toInt(num, i) {
   if (i < 0) {
     return 0;
   }
-  return num[i];
+  return num[i] - '0';
 }
 
 module.exports = function (num1, num2) {
@@ -12,26 +12,20 @@ module.exports = function (num1, num2) {
   let sums = [];
   for (let i = num1.length - 1; i >= 0; i--) {
     for (let j = num2.length - 1; j >= 0; j--) {
-      const digit = toInt(num1, i) * toInt(num2, j);
-      const key = num1.length - 1 - i + num2.length - 1 - j;
-      if (key < sums.length) {
-        sums[key].push(digit);
-      } else {
-        sums.push([digit]);
-      }
+      let product = toInt(num1, i) * toInt(num2, j);
+      let key = num1.length - 1 - i + num2.length - 1 - j;
+      do {
+        if (key >= sums.length) {
+          sums.push(product);
+        } else {
+          sums[key] += product;
+        }
+        product = Math.floor(sums[key] / 10);
+        sums[key] = sums[key] % 10;
+        key++;
+      } while (product !== 0)
     }
   }
-  let result = '';
-  let carry = 0;
-  for (let each of sums) {
-    carry += each.reduce((prev, curr) => prev + parseInt(curr), 0);
-    const digit = (carry % 10).toString();
-    result = digit + result;
-    carry = Math.floor(carry / 10);
-  }
-  if (carry != 0) {
-    result = carry + result;
-  }
-  return result;
+  return sums.reverse().join('');
 };
 
