c2ebd83a6024e90b924dc2e3c8b2946bf33635b8
Scott Kao <scottxxx666@gmail.com>
2019-11-07 02:58:30 +0800

    Iterator solution

--- spiralMatrix/spiralOrder.js
+++ spiralMatrix/spiralOrder.js
@@ -1,38 +1,30 @@
-function isFailed(i, matrix, j) {
-  return i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] === null;
-}
-
-function spiral(matrix, i, j, result, direct) {
-  result.push(matrix[i][j]);
-  matrix[i][j] = null;
+module.exports = function spiralOrder(matrix) {
+  if (matrix.length === 0) {
+    return [];
+  }
+  let result = [];
   const step = [
     { i: 0, j: 1 },
     { i: 1, j: 0 },
     { i: 0, j: -1 },
     { i: -1, j: 0 },
   ];
-  let nextI = i + step[direct].i;
-  let nextJ = j + step[direct].j;
-  let count = 0;
-  while (isFailed(nextI, matrix, nextJ)) {
-    direct = (direct + 1) % 4;
-    count++;
-    nextI = i + step[direct].i;
-    nextJ = j + step[direct].j;
-    if (count >= 4) {
-      return;
-    }
-  }
-  spiral(matrix, nextI, nextJ, result, direct);
-}
-
-module.exports = function spiralOrder(matrix) {
   let i = 0;
   let j = 0;
-  let result = [];
-  if (matrix.length === 0) {
-    return [];
+  let direct = 0;
+  for (let k = 0; k < matrix.length * matrix[0].length; k++) {
+    result.push(matrix[i][j]);
+    matrix[i][j] = null;
+    let nextI = i + step[direct].i;
+    let nextJ = j + step[direct].j;
+    if (nextI < 0 || nextI >= matrix.length || nextJ < 0 || nextJ >= matrix[0].length || matrix[nextI][nextJ] === null) {
+      direct = (direct + 1) % 4;
+      i = i + step[direct].i;
+      j = j + step[direct].j;
+    } else {
+      i = nextI;
+      j = nextJ;
+    }
   }
-  spiral(matrix, i, j, result, 0);
   return result;
 };
