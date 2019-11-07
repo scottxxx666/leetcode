ec9dd32e666fdcde2f0bd4c6ebf296c15a75ad5c
Scott Kao <scottxxx666@gmail.com>
2019-11-07 17:08:45 +0800

    Binary solution

--- pow/myPow.js
+++ pow/myPow.js
@@ -2,9 +2,16 @@ module.exports = function divide(x, n) {
   if (n === 0) {
     return 1;
   }
-  let result = 1;
+
+  let result = x;
+  let count = 1;
   let absN = Math.abs(n);
-  for (let i = 0; i < absN; i++) {
+  while (2 * count <= absN) {
+    result *= result;
+    count += count;
+  }
+  const mod = absN - count;
+  for (let i = 0; i < mod; i++) {
     result *= x;
   }
   return n > 0 ? result : 1 / result;
