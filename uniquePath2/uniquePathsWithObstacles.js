5c85ee25fbc48a388b618cd4186d85db59578bc1
Scott Kao <scottxxx666@gmail.com>
2019-11-18 18:20:42 +0800

    Space n solution

--- uniquePath2/uniquePathsWithObstacles.js
+++ uniquePath2/uniquePathsWithObstacles.js
@@ -1,23 +1,15 @@
 module.exports = function divide(obstacleGrid) {
-  const dp = new Array(obstacleGrid.length);
-  for (let i = 0; i < obstacleGrid.length; i++) {
-    dp[i] = new Array(obstacleGrid[0].length);
-  }
+  const dp = new Array(obstacleGrid[0].length).fill(0);
+  dp[0] = 1;
 
   for (let i = 0; i < obstacleGrid.length; i++) {
     for (let j = 0; j < obstacleGrid[0].length; j++) {
       if (obstacleGrid[i][j] === 1) {
-        dp[i][j] = 0;
-        continue;
+        dp[j] = 0;
+      } else if (j !== 0) {
+        dp[j] += dp[j - 1];
       }
-      if (i === 0 && j === 0) {
-        dp[i][j] = 1;
-        continue;
-      }
-      const left = j === 0 ? 0 : dp[i][j - 1];
-      const up = i === 0 ? 0 : dp[i - 1][j];
-      dp[i][j] = left + up;
     }
   }
-  return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+  return dp[obstacleGrid[0].length - 1];
 };
