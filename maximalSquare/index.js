3b768ab7c124c23a2d90d57a5af2d35174af9bd9
Scott Kao <scottxxx666@gmail.com>
2020-04-03 17:36:28 +0800

    [refactor] add dp length for decreasing handling edge case

--- maximalSquare/index.js
+++ maximalSquare/index.js
@@ -2,19 +2,15 @@ module.exports = (matrix) => {
   if (matrix.length === 0) {
     return 0;
   }
-  const dp = new Array(matrix[0].length).fill(0);
+  const dp = new Array(matrix[0].length + 1).fill(0);
 
   let max = 0;
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 1; i <= matrix.length; i++) {
     let prev = 0;
-    for (let j = 0; j < matrix[0].length; j++) {
+    for (let j = 1; j <= matrix[0].length; j++) {
       const temp = dp[j];
-      if (matrix[i][j] === "1") {
-        if (dp[j] > 0 && (j === 1 || dp[j - 1]) > 0 && prev > 0) {
-          dp[j] = Math.min(dp[j], dp[j - 1], prev) + 1;
-        } else {
-          dp[j] = 1;
-        }
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[j] = Math.min(dp[j], dp[j - 1], prev) + 1;
       } else {
         dp[j] = 0;
       }
