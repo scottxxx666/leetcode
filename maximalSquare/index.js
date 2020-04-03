fb079db616c67073e3b4f07ddff087775ac68586
Scott Kao <scottxxx666@gmail.com>
2020-04-03 17:29:18 +0800

    [refactor] space O(n)

--- maximalSquare/index.js
+++ maximalSquare/index.js
@@ -2,37 +2,25 @@ module.exports = (matrix) => {
   if (matrix.length === 0) {
     return 0;
   }
-  const dp = [];
-  for (let i = 0; i < matrix.length; i++) {
-    dp.push(new Array(matrix[0].length).fill(0));
-  }
+  const dp = new Array(matrix[0].length).fill(0);
 
-  for (let i = 0; i < matrix.length; i++) {
-    if (matrix[i][0] === "1") {
-      dp[i][0] = 1;
-    }
-  }
-  for (let i = 0; i < matrix[0].length; i++) {
-    if (matrix[0][i] === '1') {
-      dp[0][i] = 1;
-    }
-  }
-
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 1; j < matrix[0].length; j++) {
-      if (matrix[i][j] === "0") {
-        continue;
-      }
-      if (dp[i - 1][j] > 0 && dp[i][j - 1] > 0 && dp[i - 1][j - 1] > 0) {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-      } else {
-        dp[i][j] = 1;
-      }
-    }
-  }
   let max = 0;
-  for (let i = 0; i < dp.length; i++) {
-    max = Math.max(max, ...dp[i]);
+  for (let i = 0; i < matrix.length; i++) {
+    let prev = 0;
+    for (let j = 0; j < matrix[0].length; j++) {
+      const temp = dp[j];
+      if (matrix[i][j] === "1") {
+        if (dp[j] > 0 && (j === 1 || dp[j - 1]) > 0 && prev > 0) {
+          dp[j] = Math.min(dp[j], dp[j - 1], prev) + 1;
+        } else {
+          dp[j] = 1;
+        }
+      } else {
+        dp[j] = 0;
+      }
+      prev = temp;
+    }
+    max = Math.max(max, ...dp);
   }
   return max ** 2;
 };
