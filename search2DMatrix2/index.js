4cfd1d0c51097cb69aca69b1e7dd995dac23e61f
Scott Kao <scottxxx666@gmail.com>
2020-04-04 15:44:14 +0800

    [refactor] linear binary search

--- search2DMatrix2/index.js
+++ search2DMatrix2/index.js
@@ -1,28 +1,26 @@
-function dfs(matrix, target, i, j, already) {
-  if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
-    return false;
+function search(matrix, target, row) {
+  let start = 0;
+  let end = matrix[0].length;
+  let index = start;
+  while (start <= end) {
+    index = Math.floor((start + end) / 2);
+    let temp = matrix[row][index];
+    if (temp === target) {
+      return true;
+    }
+    if (temp > target) {
+      end = index - 1;
+    } else {
+      start = index + 1;
+    }
   }
-  if (already[i][j]) {
-    return false;
-  }
-  if (matrix[i][j] === target) {
-    return true;
-  }
-  already[i][j] = true;
-  if (matrix[i][j] < target) {
-    return dfs(matrix, target, i + 1, j, already) || dfs(matrix, target, i, j + 1, already);
-  } else {
-    return dfs(matrix, target, i - 1, j, already) || dfs(matrix, target, i, j - 1, already);
-  }
+  return false;
 }
 
 module.exports = (matrix, target) => {
-  if (matrix.length <= 0) {
-    return false;
-  }
-  const already = [];
+  let result = false;
   for (let i = 0; i < matrix.length; i++) {
-    already.push(new Array(matrix[0].length).fill(false));
+    result = result || search(matrix, target, i);
   }
-  return dfs(matrix, target, 0, 0, already);
+  return result;
 };
