684806c6d8bb9d3e369bcb17bf4e4d14cc5aeeb5
Scott Kao <scottxxx666@gmail.com>
2020-04-06 15:27:11 +0800

    [refactor] not need count

--- longestIncreasingSubsequence/index.js
+++ longestIncreasingSubsequence/index.js
@@ -1,15 +1,15 @@
-function dfs(nums, index, prev, count) {
+function dfs(nums, index, prev) {
   if (index > nums.length) {
-    return count;
+    return 0;
   }
   let has = 0;
   if (nums[index] > prev) {
-    has = dfs(nums, index + 1, nums[index], count + 1);
+    has = dfs(nums, index + 1, nums[index]) + 1;
   }
-  const not = dfs(nums, index + 1, prev, count);
+  const not = dfs(nums, index + 1, prev);
   return Math.max(has, not);
 }
 
 module.exports = (nums) => {
-  return dfs(nums, 0, Number.MIN_SAFE_INTEGER, 0);
+  return dfs(nums, 0, Number.MIN_SAFE_INTEGER);
 };
