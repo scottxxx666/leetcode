7ed14d6c45c8b5d162446153f31cd35c192e1771
Scott Kao <scottxxx666@gmail.com>
2020-04-06 16:43:26 +0800

    [refactor] dp

--- longestIncreasingSubsequence/index.js
+++ longestIncreasingSubsequence/index.js
@@ -1,25 +1,14 @@
-function dfs(nums, index, prevIndex, dp) {
-  if (index >= nums.length) {
+module.exports = (nums) => {
+  if (nums.length === 0) {
     return 0;
   }
-  if (dp[index + 1][prevIndex + 1] > 0) {
-    return dp[index + 1][prevIndex + 1];
+  const dp = new Array(nums.length + 1).fill(1);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i + 1] = Math.max(dp[i + 1], dp[j + 1] + 1);
+      }
+    }
   }
-  let has = 0;
-  let prev = prevIndex >= 0 ? nums[prevIndex] : Number.MIN_SAFE_INTEGER;
-  if (nums[index] > prev) {
-    has = dfs(nums, index + 1, index, dp) + 1;
-  }
-  const not = dfs(nums, index + 1, prevIndex, dp);
-  let result = Math.max(has, not);
-  dp[index + 1][prevIndex + 1] = result;
-  return result;
-}
-
-module.exports = (nums) => {
-  const dp = [];
-  for (let i = 0; i <= nums.length; i++) {
-    dp.push(new Array(nums.length + 1).fill(0));
-  }
-  return dfs(nums, 0, -1, dp);
+  return Math.max(...dp);
 };
