21e3b73c297ba50315039d072bee80d5e7e9549c
Scott Kao <scottxxx666@gmail.com>
2020-04-06 15:47:23 +0800

    [refactor] save previous result

--- longestIncreasingSubsequence/index.js
+++ longestIncreasingSubsequence/index.js
@@ -1,15 +1,25 @@
-function dfs(nums, index, prev) {
-  if (index > nums.length) {
+function dfs(nums, index, prevIndex, dp) {
+  if (index >= nums.length) {
     return 0;
   }
-  let has = 0;
-  if (nums[index] > prev) {
-    has = dfs(nums, index + 1, nums[index]) + 1;
+  if (dp[index + 1][prevIndex + 1] > 0) {
+    return dp[index + 1][prevIndex + 1];
   }
-  const not = dfs(nums, index + 1, prev);
-  return Math.max(has, not);
+  let has = 0;
+  let prev = prevIndex >= 0 ? nums[prevIndex] : Number.MIN_SAFE_INTEGER;
+  if (nums[index] > prev) {
+    has = dfs(nums, index + 1, index, dp) + 1;
+  }
+  const not = dfs(nums, index + 1, prevIndex, dp);
+  let result = Math.max(has, not);
+  dp[index + 1][prevIndex + 1] = result;
+  return result;
 }
 
 module.exports = (nums) => {
-  return dfs(nums, 0, Number.MIN_SAFE_INTEGER);
+  const dp = [];
+  for (let i = 0; i <= nums.length; i++) {
+    dp.push(new Array(nums.length + 1).fill(0));
+  }
+  return dfs(nums, 0, -1, dp);
 };
