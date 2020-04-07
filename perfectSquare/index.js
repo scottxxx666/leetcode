3bb973a8a3dc7382d48b4bf301f9b79a455c0185
Scott Kao <scottxxx666@gmail.com>
2020-04-08 01:22:02 +0800

    [refactor] O(n**2/3)

--- perfectSquare/index.js
+++ perfectSquare/index.js
@@ -1,20 +1,10 @@
-function generateSquares(n) {
-  const result = [];
-  for (let i = 1; i ** 2 <= n; i++) {
-    result.push(i ** 2);
-  }
-  return result;
-}
-
 module.exports = (n) => {
-  const squares = generateSquares(n);
   const dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
-  for (let each of squares) {
-    dp[each] = 1;
-  }
+
+  dp[0] = 0;
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j < i; j++) {
-      dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
     }
   }
   return dp[dp.length - 1];
