e54216281dbfd1d067e90c70b2679476d1aca76c
Scott Kao <scottxxx666@gmail.com>
2020-04-08 19:52:44 +0800

    [refactor] more easy solution

--- middleOfLinkList/index.js
+++ middleOfLinkList/index.js
@@ -1,26 +1,11 @@
 const ListNode = require('./listNode');
 
 module.exports = (head) => {
-  let length = 0;
-  let prev = new ListNode(null);
-  prev.next = head;
-  let pointer = prev.next;
-  while (pointer !== null) {
-    length++;
-    const temp = prev;
-    prev = pointer;
-    pointer = pointer.next;
-    prev.next = temp;
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
-
-  pointer = prev;
-  prev = null;
-  length = Math.ceil(length / 2);
-  for (let i = length; i > 0; i--) {
-    const temp = prev;
-    prev = pointer;
-    pointer = pointer.next;
-    prev.next = temp;
-  }
-  return prev;
+  return slow;
 };
