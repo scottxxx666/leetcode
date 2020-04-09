9c822ab1ab098b77729e196a72b3f004efae6fda
Scott Kao <scottxxx666@gmail.com>
2020-04-09 18:02:45 +0800

    [refactor] less space

--- backspaceStringCompare/index.js
+++ backspaceStringCompare/index.js
@@ -1,27 +1,14 @@
-function afterBackspace(s) {
-  let temp = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '#') {
-      temp = temp === 0 ? 0 : temp - 1;
-    } else {
-      s[temp] = s[i];
-      temp++;
-    }
-  }
-  return temp;
-}
-
 module.exports = (S, T) => {
-  const s = S.split('');
-  const t = T.split('');
-  let tempS = afterBackspace(s);
-  let tempT = afterBackspace(t);
-  if (tempS !== tempT) {
-    return false;
-  }
-  for (let i = 0; i < tempT; i++) {
-    if (s[i] !== t[i]) {
-      return false;
+  let i, j;
+  for (i = S.length - 1, j = T.length - 1; i >= 0 || j >= 0; i--, j--) {
+    for (let back = 0; i >= 0 && (S[i] === '#' || back); i--) {
+      back += S[i] === '#' ? 1 : -1;
+    }
+    for (let back = 0; j >= 0 && (T[j] === '#' || back); j--) {
+      back += T[j] === '#' ? 1 : -1;
+    }
+    if (i < 0 || j < 0 || S[i] !== T[j]) {
+      return i === -1 && j === -1;
     }
   }
   return true;
