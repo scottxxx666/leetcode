a32818dc91b272d519a41c6fb39786aced239ddd
Scott Kao <scottxxx666@gmail.com>
2020-04-11 03:30:57 +0800

    [refactor] in-place solution

--- gameOfLife/index.js
+++ gameOfLife/index.js
@@ -6,7 +6,7 @@ function countNext(board, i, j) {
       if (k < 0 || k >= board.length || l < 0 || l >= col || (k === i && l === j)) {
         continue;
       }
-      if (board[k][l] === 1) {
+      if ([1, 3, 4].includes(board[k][l])) {
         neighbor++;
       }
     }
@@ -17,24 +17,20 @@ function countNext(board, i, j) {
 module.exports = (board) => {
   const row = board.length;
   const col = board[0].length;
-  const result = new Array(row);
-  for (let i = 0; i < row; i++) {
-    result[i] = new Array(col).fill(0);
-  }
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
       let neighbor = countNext(board, i, j);
       if (board[i][j] === 1) {
-        result[i][j] = neighbor > 3 || neighbor < 2 ? 0 : 1;
+        board[i][j] = neighbor > 3 || neighbor < 2 ? 4 : 3;
       } else {
-        result[i][j] = neighbor === 3 ? 1 : 0;
+        board[i][j] = neighbor === 3 ? 5 : 6;
       }
     }
   }
 
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
-      board[i][j] = result[i][j];
+      board[i][j] %= 2;
     }
   }
 };
