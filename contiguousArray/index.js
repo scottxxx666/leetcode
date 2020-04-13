b0601f975ac59ccf2452e8ebc24b139a1725e502
Scott Kao <scottxxx666@gmail.com>
2020-04-13 18:46:32 +0800

    [refactor] space and time O(n)

--- contiguousArray/index.js
+++ contiguousArray/index.js
@@ -1,17 +1,14 @@
 module.exports = (nums) => {
   let maxLength = 0;
-  const dp = new Array(nums.length).fill(0);
+  const map = new Map();
+  let count = 0;
+  map.set(0, -1);
   for (let i = 0; i < nums.length; i++) {
-    const count = [0, 0];
-    count[nums[i]]++;
-    dp[i] = count;
-    for (let j = 0; j < i; j++) {
-      const count = [...dp[j]];
-      count[nums[i]]++;
-      dp[j] = count;
-      if (count[0] === count[1]) {
-        maxLength = Math.max(maxLength, count[0] * 2);
-      }
+    count += nums[i] === 1 ? 1 : -1;
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count));
+    } else {
+      map.set(count, i);
     }
   }
   return maxLength;
