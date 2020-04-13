4833af5dfcee50b3d32d7301ac31b6cb6af6e847
Scott Kao <scottxxx666@gmail.com>
2020-04-13 17:40:13 +0800

    [refactor] dp

--- contiguousArray/index.js
+++ contiguousArray/index.js
@@ -1,13 +1,21 @@
 module.exports = (nums) => {
   let maxLength = 0;
+  const dp = new Array(nums.length).fill(null);
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j <= i; j++) {
-      const count = new Array(2).fill(0);
-      for (let k = j; k <= i; k++) {
-        count[nums[k]]++;
-        if (count[0] === count[1]) {
-          maxLength = Math.max(maxLength, count[0] * 2);
-        }
+    dp[i] = new Array(nums.length).fill(0);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const count = [0, 0];
+    count[nums[i]]++;
+    dp[i][i] = count;
+  }
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const count = [...dp[i - 1][j]];
+      count[nums[i]]++;
+      dp[i][j] = count;
+      if (count[0] === count[1]) {
+        maxLength = Math.max(maxLength, count[0] * 2);
       }
     }
   }
