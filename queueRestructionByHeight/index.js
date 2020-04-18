4966486d6566a73b3f118c299640c0449447e9eb
Scott Kao <scottxxx666@gmail.com>
2020-04-19 03:08:03 +0800

    [refactor] brilliant solution

--- queueRestructionByHeight/index.js
+++ queueRestructionByHeight/index.js
@@ -1,16 +1,8 @@
 module.exports = (people) => {
-  let orders = people.sort((a, b) => (a[1] - b[1]) * 10 + b[0] - a[0]);
+  let orders = people.sort((a, b) => (b[0] - a[0]) * 10 + a[1] - b[1]);
   let result = [];
   for (let each of orders) {
-    let count = 0;
-    let i = 0;
-    while (count < each[1]) {
-      if (result[i][0] >= each[0]) {
-        count++;
-      }
-      i++;
-    }
-    result = [...result.slice(0, i), each, ...result.slice(i, result.length)];
+    result.splice(each[1], 0, each);
   }
   return result;
 };
