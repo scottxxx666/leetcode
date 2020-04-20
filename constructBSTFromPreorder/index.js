b666b7d410b96621bc64e06931d5a28c868d3b1c
Scott Kao <scottxxx666@gmail.com>
2020-04-20 17:18:01 +0800

    [refactor] stack solution

--- constructBSTFromPreorder/index.js
+++ constructBSTFromPreorder/index.js
@@ -1,24 +1,21 @@
 const TreeNode = require("./treeNode");
-
-function insert(head, num) {
-  if (head.val > num) {
-    if (head.left === null) {
-      head.left = new TreeNode(num);
-      return;
-    }
-    return insert(head.left, num);
-  }
-  if (head.right === null) {
-    head.right = new TreeNode(num);
-    return;
-  }
-  return insert(head.right, num);
-}
-
 module.exports = (preorder) => {
   const head = new TreeNode(preorder[0]);
+  const prev = [];
+  let pointer = head;
   for (let i = 1; i < preorder.length; i++) {
-    insert(head, preorder[i]);
+    const target = preorder[i];
+    if (target < pointer.val) {
+      pointer.left = new TreeNode(target);
+      prev.push(pointer);
+      pointer = pointer.left;
+      continue;
+    }
+    while (prev.length > 0 && prev[prev.length - 1].val < target) {
+      pointer = prev.pop();
+    }
+    pointer.right = new TreeNode(target);
+    pointer = pointer.right;
   }
   return head;
 };
