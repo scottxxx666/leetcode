71cc24e5c2079a2ed50635b61c91d5beb8d190cc
Scott Kao <scottxxx666@gmail.com>
2020-04-22 16:07:59 +0800

    [refactor] space O(n)

--- subarraySumEqualsK/index.js
+++ subarraySumEqualsK/index.js
@@ -1,19 +1,10 @@
 module.exports = (nums, k) => {
-  const dp = new Array(nums.length + 1).fill(null);
-  for (let i = 0; i < dp.length; i++) {
-    dp[i] = new Array(nums.length + 1).fill(0);
-  }
-
   let result = 0;
-  for (let i = 1; i < dp.length; i++) {
-    for (let j = 1; j <= i; j++) {
-      if (i === j) {
-        dp[i][j] = nums[i - 1];
-      } else {
-        dp[i][j] = dp[i - 1][j] + nums[i - 1];
-      }
-
-      if (dp[i][j] === k) {
+  for (let i = 1; i <= nums.length; i++) {
+    const dp = new Array(nums.length + 1).fill(0);
+    for (let j = i; j <= nums.length; j++) {
+      dp[j] = dp[j - 1] + nums[j - 1];
+      if (dp[j] === k) {
         result++;
       }
     }
