2edcc1d61320685af48ef67a1969fb6e5727400c
Scott Kao <scottxxx666@gmail.com>
2020-04-23 16:47:59 +0800

    [refactor] more readable

--- bitwiseAndOfNumbersRange/index.js
+++ bitwiseAndOfNumbersRange/index.js
@@ -1,12 +1,3 @@
 module.exports = function rangeBitwiseAnd(m, n) {
-  if (m === 0) {
-    return 0;
-  }
-  let bitMove = 1;
-  while (m !== n) {
-    m >>= 1;
-    n >>= 1;
-    bitMove <<= 1;
-  }
-  return m * bitMove;
+  return m === n ? m : rangeBitwiseAnd(m >> 1, n >> 1) << 1;
 };
