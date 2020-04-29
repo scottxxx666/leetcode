46f7b6533b92fe42deca71f24eaad991ab66038d
Scott Kao <scottxxx666@gmail.com>
2020-04-29 16:18:47 +0800

    [refactor] more readable

--- BinaryTreeMaximumPathSum/index.js
+++ BinaryTreeMaximumPathSum/index.js
@@ -1,22 +1,19 @@
 module.exports = function maxPathSum(root) {
+  let max = Number.MIN_SAFE_INTEGER;
+
   function calculate(pointer) {
     if (pointer === null) {
-      return { max: Number.MIN_SAFE_INTEGER, sum: 0 };
+      return 0;
     }
     const left = calculate(pointer.left);
     const right = calculate(pointer.right);
-    return {
-      max: Math.max(
-        left.max,
-        right.max,
-        left.sum + right.sum + pointer.val,
-        pointer.val,
-        pointer.val + left.sum,
-        pointer.val + right.sum,
-      ),
-      sum: Math.max(left.sum + pointer.val, right.sum + pointer.val, pointer.val),
-    };
+
+    const val = pointer.val;
+    const sum = Math.max(val + left, val + right, val);
+    max = Math.max(max, sum, val + left + right);
+    return sum;
   }
 
-  return calculate(root).max;
+  calculate(root);
+  return max;
 };
