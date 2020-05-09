50c240b3941986004b5719b86213b646d1dee8aa
Scott Kao <scottxxx666@gmail.com>
2020-05-09 17:00:49 +0800

    [refactor] readable

--- validPerfectSquare/index.js
+++ validPerfectSquare/index.js
@@ -1,15 +1,13 @@
 module.exports = function (num) {
   let left = 1;
   let right = num;
-  while (left <= right) {
+  while (left < right) {
     const target = Math.floor((left + right) / 2);
-    if (target ** 2 === num) {
-      return true;
-    } else if (target ** 2 > num) {
-      right = target - 1;
+    if (target ** 2 >= num) {
+      right = target;
     } else {
       left = target + 1;
     }
   }
-  return false;
+  return left ** 2 === num;
 };
