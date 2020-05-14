f700d89d5aaccb06c53458106d418c64a4141b12
Scott Kao <scottxxx666@gmail.com>
2020-05-14 17:26:22 +0800

    [refactor] readable

--- removedKDigits/removedKDigits.js
+++ removedKDigits/removedKDigits.js
@@ -1,22 +1,15 @@
 module.exports = (num, k) => {
   const stack = [];
-  let j = 0;
   let count = 0;
-  while (j < num.length) {
-    if (count < k && stack.length > 0 && stack[stack.length - 1] > num[j]) {
+  for (let j = 0; j < num.length; j++) {
+    while (count < k && stack.length > 0 && stack[stack.length - 1] > num[j]) {
       stack.pop();
       count++;
-    } else {
-      stack.push(num[j++]);
     }
+    stack.push(num[j]);
   }
 
-  while (count < k) {
-    stack.pop();
-    count++;
-  }
-
-  let result = stack.join('');
+  let result = stack.splice(0, num.length - k).join('');
   let i = 0;
   while (result[i] === '0') {
     result = result.replace('0', '');
