bcaa9d534c1910c776f3f092771640aaa7e93e84
Scott Kao <scottxxx666@gmail.com>
2020-05-16 02:05:15 +0800

    [refactor] O(n) solution

--- maximumSumCircularSubarray/index.js
+++ maximumSumCircularSubarray/index.js
@@ -1,13 +1,20 @@
 module.exports = (A) => {
-  const a = [...A, ...A];
-  let max = Number.MIN_SAFE_INTEGER;
-  for (let start = 0; start < A.length; start++) {
-    let sum = 0;
-    for (let i = start; i <= start + A.length - 1; i++) {
-      sum += a[i];
-      sum = Math.max(a[i], sum);
-      max = Math.max(max, sum);
-    }
+  const sum = A.reduce((p, c) => p + c);
+
+  let ans1 = Number.MIN_SAFE_INTEGER;
+  let temp = 0;
+  for (let i = 0; i < A.length; i++) {
+    temp = A[i] + Math.max(0, temp);
+    ans1 = Math.max(ans1, temp);
   }
-  return max;
+
+  let min = Number.MAX_SAFE_INTEGER;
+  temp = Number.MAX_SAFE_INTEGER;
+  for (let i = 1; i < A.length - 1; i++) {
+    temp = A[i] + Math.min(0, temp);
+    min = Math.min(temp, min);
+  }
+  const ans2 = sum - min;
+
+  return Math.max(ans1, ans2);
 };
