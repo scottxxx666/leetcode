48db68e79bdf3e5a1cdccb9d1fa355d9b3339d71
Scott Kao <scottxxx666@gmail.com>
2020-05-18 01:20:38 +0800

    [refactor] O(n) solution

--- findAllAnagramsInAString/index.js
+++ findAllAnagramsInAString/index.js
@@ -1,35 +1,38 @@
+function getKey(s) {
+  return s.charCodeAt(0) - 'a'.charCodeAt(0);
+}
+
 module.exports = (s, p) => {
-  const chars = new Array(26).fill(0);
-  for (let c of p) {
-    chars[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+  const counts = new Array(26).fill(0);
+  for (let each of p) {
+    const key = getKey(each);
+    counts[key]++;
   }
 
   const result = [];
-  let candidates = [];
-  for (let i = 0; i < s.length; i++) {
-    const c = s[i];
-    let temp = [];
-    const key = c.charCodeAt(0) - 'a'.charCodeAt(0);
-    for (let candidate of candidates) {
-      if (candidate[key] > 0) {
-        candidate[key]--;
-        if (candidate.every(e => e === 0)) {
-          result.push(i - p.length + 1);
-        } else {
-          temp.push(candidate);
-        }
-      }
+  let left = 0;
+  let right = 0;
+  let length = 0;
+  while (right < s.length) {
+    const key = getKey(s[right]);
+    counts[key]--;
+    right++;
+    if (counts[key] >= 0) {
+      length++;
     }
-    const newCandidate = [...chars];
-    if (newCandidate[key] > 0) {
-      newCandidate[key]--;
-      if (newCandidate.every(e => e === 0)) {
-        result.push(i);
-      } else {
-        temp.push(newCandidate);
-      }
+
+    if (length === p.length) {
+      result.push(left);
     }
-    candidates = temp;
+
+    if (right - left >= p.length) {
+      const key = getKey(s[left]);
+      counts[key]++;
+      if (counts[key] >= 1) {
+        length--;
+      }
+      left++;
+    }
   }
   return result;
 };
--- findAllAnagramsInAString/index.test.js
+++ findAllAnagramsInAString/index.test.js
@@ -2,4 +2,7 @@ const findAnagrams = require("./index");
 
 test('simple', function () {
   expect(findAnagrams("cbaebabacd", "abc")).toStrictEqual([0, 6]);
-}); 
+});
+test('simple', function () {
+  expect(findAnagrams("baa", "aa")).toStrictEqual([1]);
+});
