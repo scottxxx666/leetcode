fc68578f2d2f3adab51b8923187fcd4d971b7fc4
Scott Kao <scottxxx666@gmail.com>
2020-05-21 16:29:36 +0800

    [refactor] remove redundant code

--- countSquareSubmatricesWithAllOnes/index.js
+++ countSquareSubmatricesWithAllOnes/index.js
@@ -7,15 +7,10 @@ module.exports = (matrix) => {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
-      if (matrix[i][j] === 0) {
-        continue;
-      }
-      if (dp[i][j + 1] > 0 && dp[i + 1][j] > 0) {
+      if (matrix[i][j] > 0) {
         dp[i + 1][j + 1] = Math.min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1;
-      } else {
-        dp[i + 1][j + 1] = 1;
+        sum += dp[i + 1][j + 1];
       }
-      sum += dp[i + 1][j + 1];
     }
   }
 
