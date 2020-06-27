24793a744ddf8c5c6bd2ccb53f984f5badf7abbf
Scott Kao <scottxxx666@gmail.com>
2020-06-28 03:00:24 +0800

    [refactor] simplify logic

--- minimumSizeSubarraySum/index.js
+++ minimumSizeSubarraySum/index.js
@@ -4,12 +4,10 @@ const minSubArrayLen = function (s, nums) {
   let result = Number.MAX_SAFE_INTEGER;
   for (let end = 0; end < nums.length; end++) {
     sum += nums[end];
-    while (start < end && (sum - nums[start]) >= s) {
+    while (sum >= s) {
+      result = Math.min(result, end - start + 1);
       sum -= nums[start];
       start++;
-    }
-    if (sum >= s) {
-      result = Math.min(result, end - start + 1);
     }
   }
   return result === Number.MAX_SAFE_INTEGER ? 0 : result;
