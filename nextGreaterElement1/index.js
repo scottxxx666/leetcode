9aa77188167261d39679dd72b216475dcf80de8b
Scott Kao <scottxxx666@gmail.com>
2020-06-30 00:38:09 +0800

    [refactor] O(n) solution

--- nextGreaterElement1/index.js
+++ nextGreaterElement1/index.js
@@ -1,12 +1,14 @@
 const nextGreaterElement = function (nums1, nums2) {
   const map = new Map();
+  const stack = [];
   map.set(nums2[nums2.length - 1], -1);
+  stack.push(nums2[nums2.length - 1]);
   for (let i = nums2.length - 2; i >= 0; i--) {
-    let nextGreater = nums2[i + 1];
-    while (nums2[i] > nextGreater && nextGreater !== -1) {
-      nextGreater = map.get(nextGreater);
+    while (stack.length > 0 && nums2[i] > stack[stack.length - 1]) {
+      stack.pop();
     }
-    map.set(nums2[i], nextGreater);
+    map.set(nums2[i], stack.length ? stack[stack.length - 1] : -1);
+    stack.push(nums2[i]);
   }
 
   const result = [];
