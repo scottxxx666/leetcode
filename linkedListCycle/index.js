b2c7cbe88492f3e6e5c22b0663699fce682c268b
Scott Kao <scottxxx666@gmail.com>
2020-07-01 02:03:21 +0800

    [feat] linked list cycle

---
+++ linkedListCycle/index.js
@@ -0,0 +1,12 @@
+const hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
+};
