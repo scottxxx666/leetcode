3eb0a5d2a2fd12a5d34e5990ba31fef084c3de0f
Scott Kao <scottxxx666@gmail.com>
2020-07-09 16:53:23 +0800

    [refactor] readable

--- mergeIntervals/merge.js
+++ mergeIntervals/merge.js
@@ -1,22 +1,20 @@
 function merge(intervals) {
   if (intervals.length === 0) {
-    return intervals;
+    return [];
   }
-  let sorted = intervals.sort((a, b) => a[0] - b[0]);
-  let result = [sorted[0]];
-  for (let i = 1; i < sorted.length; i++) {
-    let begin = sorted[i][0];
-    let end = sorted[i][1];
-    const last = result[(result.length - 1)];
-    if ((begin < last[0] && end < last[0]) || (begin > last[1] && end > last[1])) {
-      result.push([begin, end]);
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const result = [];
+  let [start, end] = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    if (end >= intervals[i][0]) {
+      end = Math.max(end, intervals[i][1]);
     } else {
-      begin = Math.min(begin, last[0]);
-      end = Math.max(end, last[1]);
-      result[result.length - 1] = [begin, end];
+      result.push([start, end]);
+      [start, end] = intervals[i];
     }
   }
+  result.push([start, end]);
   return result;
 }
-
 module.exports = merge;
