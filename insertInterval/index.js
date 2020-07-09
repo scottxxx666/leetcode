b2165a9f714baa8c419080a79926308eb791e8d4
Scott Kao <scottxxx666@gmail.com>
2020-07-09 17:26:04 +0800

    [refactor] O(n) solution

--- insertInterval/index.js
+++ insertInterval/index.js
@@ -1,15 +1,15 @@
 const insert = function (intervals, newInterval) {
-  intervals.push(newInterval);
-  intervals.sort((a, b) => a[0] - b[0]);
-
   const result = [];
-  let [start, end] = intervals[0];
-  for (let i = 1; i < intervals.length; i++) {
-    if (end >= intervals[i][0]) {
-      end = Math.max(end, intervals[i][1]);
-    } else {
+  let [start, end] = newInterval;
+  for (let i = 0; i < intervals.length; i++) {
+    if (start > intervals[i][1]) {
+      result.push(intervals[i]);
+    } else if (end < intervals[i][0]) {
       result.push([start, end]);
       [start, end] = intervals[i];
+    } else {
+      start = Math.min(start, intervals[i][0]);
+      end = Math.max(end, intervals[i][1]);
     }
   }
 
