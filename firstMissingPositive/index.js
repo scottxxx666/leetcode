a4d375cfcf85d1d538797b7d76c7c81da4944f99
Scott Kao <scottxxx666@gmail.com>
2020-07-15 16:35:46 +0800

    [fix] corner case

--- firstMissingPositive/index.js
+++ firstMissingPositive/index.js
@@ -1,7 +1,7 @@
 const firstMissingPositive = function (nums) {
   let i = 0;
   while (i < nums.length) {
-    if (nums[i] < nums.length && nums[i] - 1 !== i && nums[i] !== nums[nums[i] - 1]) {
+    if (nums[i] > 0 && nums[i] < nums.length && nums[i] !== nums[nums[i] - 1]) {
       swap(nums, i, nums[i] - 1);
     } else {
       i++;
